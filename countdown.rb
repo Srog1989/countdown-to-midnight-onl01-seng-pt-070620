34cb99100a30b2cbe2c88084d3fcf08ab52bde34
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-10 02:19:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,8 @@
 def countdown ("number_of_seconds!")
-  while number_of_seconds < 10
-  puts number_of_seconds
-
-def countdown
+  while number_of_seconds > 0
+  puts "{number_of_seconds}"
+  number_of_seconds -= 1
 end
+"HAPPY NEW YEAR!"
+end 
+
